5bfe541e43bc9fbb674180ed2c1734fa4a5dcb57
Ajith Senthil <ajith200515@gmail.com>
2026-03-17 13:48:05 +0530

    this is my second commit

--- student-crud/src/main/java/com/example/StudentCrud/controller/StudentController.java
+++ student-crud/src/main/java/com/example/StudentCrud/controller/StudentController.java
@@ -20,6 +20,8 @@ public class StudentController
 {
 
         private final StudentService studentService;
+        
+        
 
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
